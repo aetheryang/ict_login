05bb1cfa495f9deb57e7e757bcc156464627bdfb
aetheryang <aetheryang@gmail.com>
2016-04-13 10:24:25 +0800

    modified:   login.rb you may know how to save your password now

--- login.rb
+++ login.rb
@@ -7,3 +7,9 @@ pass  = STDIN.noecho(&:gets).chomp
 uri = URI('http://159.226.39.22/cgi-bin/do_login')
 res = Net::HTTP.post_form(uri, 'uname' => uname, 'pass'  => pass)
 puts res.body if res.body.include?('error')
+
+# ATTENTION! If you don't mind to save your password even EVERYONE can get it, change line 2 to line 9 into line 14
+# and do not forget change the uname and pass.
+#
+# Net::HTTP.post_form(URI('http://159.226.39.22/cgi-bin/do_login'), 'uname' => 'Goldjin', 'pass' => 'spicy chicken')
+#

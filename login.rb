64c08d38f7bfd9ae4182619b44c23536e6072243
aetheryang <aetheryang@gmail.com>
2016-04-14 14:11:00 +0800

    modified:   login.rb

--- login.rb
+++ login.rb
@@ -10,8 +10,8 @@ uri = URI('http://159.226.39.22/cgi-bin/do_login')
 res = Net::HTTP.post_form(uri, 'uname' => uname, 'pass'  => pass)
 puts res.body if res.body.include?('error')
 
-# ATTENTION! If you don't mind to save your password even EVERYONE can get it, change line 2 to line 9 into line 14
-# and do not forget change the uname and pass. Also you may just save your uname, find it out by yourself. :)
+# ATTENTION! If you don't mind to save your password even EVERYONE can get it,
+# do not forget change the uname and pass. Also you may just save your uname, find it out by yourself. :)
 #
 # Net::HTTP.post_form(URI('http://159.226.39.22/cgi-bin/do_login'), 'uname' => 'Goldjin', 'pass' => 'spicy chicken')
 #
